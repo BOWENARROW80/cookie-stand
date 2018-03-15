89e41d30d647b440ac2c6410df864fcc72f9832a
darryl <djbowen2@hotmail.com>
2018-03-14 23:36:44 -0700

    fixing remaining issues

--- js/app.js
+++ js/app.js
@@ -1,219 +1,258 @@
-'use strinct';
+'use strict';
 
-var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 
-// First and Pike
+ var StoreArray = []
 
- var FristAndPike = {
-  minCustPerHour: 23,
-  maxCustPerHour: 65,
-  aveCookiePerSale: 6.3
-  cookieSales: [], 
-  rumSum 0,
+ var storeTable = document.getElementById('Store')
 
-  showSales: function()
-[
+ var openHours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
+   var header = FUNCTION () {
 
+    var trElement = document.createElement('tr'):
+    var tdElement = document.createElement('td');
+
+    trElement.textContent = 'Store';
+    thElement.appendChild(trElement);
+
+    for (var i = 0; i < OpenHours.length; i++) {
+
+      theElement = document.createElement('th')
+
+      tdElement.textContent = openHours[i];
+
+      trElement.appendChild(tdElement);
+
+      {
+        tdElement = document.createElement('td');
+        tdElement.textContent = 'Day Total';
+        trElement.appendChild(tdElement);
+      }
+      }
+
+  header();
+
+  function StoreConstructor(location, custMini, custMaxi, aveCookieSale) {
+    this.location = location;
+    this.mini = custMini;
+    this.Maxi = custMaxi;
+    this.aveCookieSale = aveCookieSale;
+    this.hourlySales =[]
+    this.totalDailySales = 0;
+    storeArray.push(this); //pushes the THIS items into the array.  The StoreArray is connected from the top of the coding chain
+ 
+
+   }
+
+
+ StoreConstructor.prototype.render = function () {
+
+    //create tr
+    var trElement = document.createElement('tr');
+    //create td
+    var tdElement = document.createElement('td');
+    //give content name
+    tdElement.textContent = this.location;
+    //append td to tr
+    trElement.appendChild(tdElement);
   
-]
+    for (var i = 0; i < hourlySales.length; i++) 
+    tdElement = document.createElement('td');
+    tdElement.textContent = this.totalDailySales[i];
+    trElement.appendChild(tdElement);
+
  }
- 'use strinct';
 
- var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
- var pikeStore = cookie.store ('pike place', 16, 88, 5.2);
- var seaTacStore = cookie.store ('SeaTac Airport', 3, 24, 1.2);
- var seaCenterStore = cookie.store ('Seattle Center', 11, 38, 3.7);
- var capitolHillstore = cookie.store ('Capitol Hill', 20, 38, 2.3);
- var alki = cookie.store ('Alki', 2, 16, 4.6); 
+ 
+ StoreConstructor.prototype.storeSalesPerHr = function () {
+   
+  var totalDailySales = 0;
+  for (var i = 0; i < OpenHours.length; i++) {
 
-//store info
-function Stores (location, custMini, custMaxi, aveCookieSale) {
-  this.location = location;
-  this.custMini = custMini;
-  this.custMaxi = custMaxi;
-  this.aveCookieSale = aveCookieSale;
-  this.totalSales = 0;
-  this.hourSales = [0];
+    var cusRadoNum= Math.random() * (this.custMaxi - this.custMini) + (this.custMini);
+
+    var hourlySales = (Math.floor(cusRanoNum * this.aveSale));
+
+    dayTotal += hourlySales;
+
+    this.hourlySales.push(totalDailySales);
+
+    this.hourlySales = totalDailySales;
+
 }
-//Customers
-Stores.prototype.storeCustomer = function () {
-  return Math.random() * (this.custMaxi - this.custMini) + this.custMini;
-}
-//Sales Per Hour
-Stores.prototype.storeSalesPerHr = function () {
-  for (var i = 0; i < OpenHours.length; i++){
-    var cookieGen = math.floor(this.aveCookieSale * thisStoreRandomCustomer());
-    this.hourSales.push(randCookies);
-    this.totalSales += randCookies;
-  }
+
+ tdElement = document.createElement('td');
+ tdElement.textContent = this.totalSales
+ trElement.appendChild(tdElement);
+
+ storeTable.appendChild(trElement);
+
 };
 
 //Array for locations
-var PikeLocation = NewStore[6am, 7am 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, Daily Location Total];
-var SeatacLocation = NewStore[6am, 7am 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, Daily Location Total];
-var SeatleCLocation = NewStore[6am, 7am 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, Daily Location Total];
-var CapitolLocation = NewStore[6am, 7am 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, Daily Location Total];
-var AlkiLocation = NewStore[6am, 7am 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, Daily Location Total];
-var LocationTotal = NewStore[6am, 7am 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, Daily Location Total];
+var pike = New StoreConstructor[23, 65, 6.3. '1st and Pike'];
+pike.salesPerHr();
+pike.render();
 
+var Setac = New StoreConstructor[3, 24, 1.2 'SeaTac Airport'];
+seatac.salesPerHr();
+seatac.render();
 
+var SeaCenter = New StoreConstructor[11, 38. 3.7. 'Seattle Center'];
+SeaCenter.salesPerHr();
+SeaCenter.render();
 
-var cookieTable = document.getElementById('cookie');
-function cookie(location, time, dailySales); {
- this.location = location;
- this.time = time;
- this.dailySales = dailySales;
-allCookieLocations.push(this);
+var CapitolHill = New StoreConstructor[20, 38, 2.3. 'Capitol Hill'];
+pike.salesPerHr();
+pike.render();
 
-Cookie.prototype.render = function () {
+var Alki = New StoreConstructor[2, 16, 4.6,. 'Alki'];
+Alki.salesPerHr();
+Alki.render();
 
+//SINGLE FUNCION THAT RENDER ALL OF THE INDIVIDUAL ROWS
+function renderaAllCookies() {
+  for(var i in allCookies) {
+    allCookies[i].render();
+  }
 
 }
-//create tr
-var trElement = document.createElement('tr');
-//create td
-var tdElement = document.createElement('td');
-//give content name
-tdElement.textContent = this.location;
-//append td to tr
-trElement.appendChild(tdElement);
 
-tdElement = document.createElement('td');
-tdElement.textContent = this.time;
-trElement.appendChild(tdElement);
+// event listener's callback function
+function addNewCookie(event){
+  //EVENT RETURNS AN OBJECT
+  event.preventDefault();
+  console.log(event.target.pikeLocation.value);
 
-trElement.appendChild(tdElement);
-
-tdElement = document.createElement('td');
-tdElement.textContent = this.dailySales;
-trElement.appendChild(tdElement);
-
-//append TR to table
-cookieTable.appendChild(trElement);
-};
+  /////change these to the cookie vice dog
+  var newLocation= event.target.location.value;
+  var newCustMin = event.target.miniCust.value;
+  var newCustMax= event.target.MaxCust.value;
+  var newAveCookSale = event.target.AvgCookieSale.value
 
 
-var PikeLocation = new Cookie('6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm','5pm','6pm','7pm', 'Daily Total');
-var SeaTacLocation = new Cookie('6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm','5pm','6pm','7pm', 'Daily Total');
-var SeattleCenterLocation = new Cookie('6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm','5pm','6pm','7pm', 'Daily Total');
-var CapitolHill = new cookie('6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm','5pm','6pm','7pm', 'Daily Total');
-var Alki = new cookie('6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm','5pm','6pm','7pm', 'Daily Total');
-var total = new cookie ()
-
-// 1 // We need an array to hold our dogs
-// // var gary = newdog(gary, white, westie, gare bear);
-// // var charlote= new dog(blah, blah, blah, blah)
-
-// 2. // Let's refactor so that render() method is on the constructor's prototype; this will tidy up the way things look on the screen
+// // new Dog(newName, newColor, newBreed, newNickname); ///same order as previousl
+///location, custMini, custMaxi, aveCookieSale
+dogTable.innerHTML = '';
+makeHeaderRow();
+renderaAllCookies();
+}
 
 
-render = function (TableRows) {
-  var PikeLocation = document.getElementById('Pike');
+// // add event lstener
+// cookieForm.addEventListener('submit', addNewCookie);
+
+// // //Erases everything and moves footer then re-adds everything including new item added
+// // dogTable.innerHTML = ''; //now header is missing
 
 
 
+// <form id="cookie form">
+// <fieldset>
+// <legend>Add a new location</legend>
 
-  store.prototype.render=function () {
-var trElement = document.createElement('tr');
-var tdElement = document.createElement('td');
-tdElement.textContent = this.location
-trElement.appendChild(tdElement);
-salesTable.appendChild(trElement);
+// <label for= "cookie-name">:</label>
+// <input type="text" name-"cookie-name">
+
+// <label for= "cookie-name">Name:</label>
+// <input type="text" name-"cookie-name">
+
+// <label for= "cookie-name">Name:</label>
+// <input type="text" name-"cookie-name">
+
+// <label for= "cookie-name">Name:</label>
+// <input type="text" name-"cookie-name">
+
+// <button>Add a Store</button>
+
+// </fieldset>
+
+// </form>
+
+// <textarea name="" id="" cols="30" rows="10"></textarea>
+
+
+// </fieldset>
+
+// </form>
 
 
 
-// 3  // create tr
-// var trElement = document.createElement('tr');
+// <table id="cookie"></table>
 
-//   // create td
-// var tdElement = document.createElement('td');
+// <tr>
+//             <th>Store</th>
+//             <th></th>
+//             <th></th>
+//             <th></th>
+//           </tr>
+//         <tr>
+//             <td></td>
+//             <td>6am</td>
+//             <td>7am</td>
+//             <td>8am</td>
+//             <td>9am</td>
+//             <td>10am</td>
+//             <td>11am</td>
+//             <td>12pm</td>
+//             <td>1pm</td>
+//             <td>2pm</td>
+//             <td>3pm</td>
+//             <td>4pm</td>
+//             <td>5pm</td>
+//             <td>6pm</td>
+//             <td>7pm</td>
+//             <td>Daily Total</td>
+//           </tr>
 
-//   // give td content (name, then color, then breed, the nickname) <- eventually do it in a for loop
-// tdElement.textContent = this.name;
+     
+//       <p></p>
+//       <tr>
+//         <td>1st & Pike</td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//       </tr>
+//       <tr>
+//         <td>Seatac Airport</td>
+//         <td></td>
+//         <td></td>
+//         <td></td>
+//       </tr>
+//       <tr>
+//         <td>Seattle Center</td>
+//         <td></td>
+//         <td> </td>
+//         <td></td>
+//       </tr>
+//       <tr>
+//         <td>Capitol Hill</td>
+//         <td></td>
+//         <td>-</td>
+//         <td></td>
+//       </tr>
+//       <tr>
+//         <td>Alki</td>
+//         <td> </td>
+//         <td> </td>
+//         <td></td>
+//       </tr>
+//       <tr>
+      
+//         <td></td>
+//       </tr>
+     
+//     </table>
 
-//   // append td to tr
-// trElement.appendChild(tdElement);
 
-// tdEL = document.createELement('td');
-//   tdElement.textContent = this.color
-//   trElement.appendChild(tdElement)
-  
-//    tdEL = document.createELement('td');
-//   tdElement.textContent = this.breed
-//   trElement.appendChild(tdElement)
-  
-//    tdEL = document.createELement('td');
-//   tdElement.textContent = this.nickname
-//   trElement.appendChild(tdElement)
 
-//   // append tr to table
-// dogTable.appendChild(trElement);
 
-/* To see what we need to do to build a HTML table with JS, let's take a look at the HTML needed to start the table and make the top two rows:
-<table>                 PUT THIS AND ONLY THIS IN THE HTML, WITH AN ID
-  <tr>                  create tr
-    <th>Name</th>       create a th, give it content, append it to tr
-    <th>Color</th>      create a th, give it content, append it to tr
-    <th>Breed</th>      create a th, give it content, append it to tr
-    <th>Nickname</th>   create a th, give it content, append it to tr
-  </tr>                 append tr to the table
-  <tr>
-    <td>Gary</td>
-    <td>White</td>
-    <td>Westie</td>
-    <td>Gare Bear</td>
-  </tr>
-</table>
-Tables are built in the same way that a typewriter types: left-to-right, top-to-bottom.
-Thus, we'll want to create and append HTML elements in the same order.
-We have already put an empty <table> element in the HTML, with id="dogs" */
 
-// // We need an array to hold our dogs
-// var gary = newdog(gary, white, westie, gare bear);
-// var charlote= new dog(blah, blah, blah, blah)
 
-// We need to access the table that is in the DOM
 
-// We need a constructor to make our dog objects
 
-// // Let's refactor so that render() method is on the constructor's prototype; this will tidy up the way things look on the screen
-// render = function () {
-//   dog.prototype.render=function () {
 
-  // }
-//   // create tr
-// var trElement = document.createElement('tr');
 
-//   // create td
-// var tdElement = document.createElement('td');
 
-//   // give td content (name, then color, then breed, the nickname) <- eventually do it in a for loop
-// tdElement.textContent = this.name;
 
-//   // append td to tr
-// trElement.appendChild(tdElement);
 
-// tdEL = document.createELement('td');
-//   tdElement.textContent = this.color
-//   trElement.appendChild(tdElement)
-  
-//    tdEL = document.createELement('td');
-//   tdElement.textContent = this.breed
-//   trElement.appendChild(tdElement)
-  
-//    tdEL = document.createELement('td');
-//   tdElement.textContent = this.nickname
-//   trElement.appendChild(tdElement)
 
-//   // append tr to table
-// dogTable.appendChild(trElement);
-
-// };
-
-// We need to create our Dog instances
-
-// We need a separate function to make the table header
-
-// It would be nice to have a single function that renders all of the individual dog rows
-
-// Now we need to call our functions: the one for the header row, and the one for generating the individual dog rows
